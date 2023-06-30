1c4a03e65c95e70742b89ab6d86ea0c84e06d57f
Julyanne Correia <julyanne.es@gmail.com>
2023-06-30 17:59:17 -0300

    feat: average, turn_around_time and init

--- processing_simulator/process_manager.py
+++ processing_simulator/process_manager.py
@@ -3,6 +3,9 @@ from process import HighLevelProcess
 class HighLevelProcessManager():
     def __init__(self, processes: list[HighLevelProcess]) -> None:
         self.queue_ready = processes
+        self.processes_amount = len(processes)
+        self.wainting_time = [0] * self.processes_amount
+        self.turn_around_time = [0] * self.processes_amount
 
     def put_into_queue_ready(self, process: HighLevelProcess) -> None:
         """Put a process into the ready queue.
@@ -38,3 +41,30 @@ class HighLevelProcessManagerRR(HighLevelProcessManager):
     def __init__(self, processes) -> None:
         super().__init__(processes)
 
+    def start(self):
+        if self.queue_ready == []:
+            print("No process in ready queue")
+            return None
+
+    def __find_waiting_time__(self, quantum):
+        pass
+
+    def __find_turn_around_time__(self):
+
+        for i in range(self.processes_amount):
+
+            self.turn_around_time[i] = self.queue_ready[i].burst_time + self.wainting_time[i]
+
+    def __find_average_time__(self):
+        waiting_total_time = 0
+        ramaining_total_time = 0
+
+        self.__find_waiting_time__(self, self.queue_ready[0].quantum_time)
+        self.__find_turn_around_time__(self)
+
+        for i in range(self.processes_amount):
+            waiting_total_time += self.wainting_time[i]
+            ramaining_total_time += self.turn_around_time[i]
+
+        print("\nAverage waiting time = %.5f "%(waiting_total_time / self.processes_amount) )
+        print("Average turn around time = %.5f "% (ramaining_total_time / self.processes_amount))
