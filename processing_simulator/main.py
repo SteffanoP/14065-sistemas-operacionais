79d2b7e44a21c6a8513fe582ebc0a4bc2dfd6e67
SteffanoP <steffanoxpereira@gmail.com>
2023-07-01 21:06:41 -0300

    refactor: abandon Process from multithreading
    No need for multithreading here

--- processing_simulator/main.py
+++ processing_simulator/main.py
@@ -1,9 +1,27 @@
 from process import HighLevelProcess
 from process_manager import HighLevelProcessManagerRR, HighLevelProcessManagerSRTF
 
-process_manager_srtf = HighLevelProcessManagerSRTF()
-process_manager_rr = HighLevelProcessManagerRR()
+my_process_1 = HighLevelProcess(
+    pid=1,
+    name="Process 1",
+    priority=0,
+    is_io_bound=False,
+    burst_time=7
+)
+my_process_2 = HighLevelProcess(
+    pid=2,
+    name="Process 2",
+    priority=0,
+    is_io_bound=False,
+    burst_time=10
+)
+my_process_3 = HighLevelProcess(
+    pid=3,
+    name="Process 3",
+    priority=0,
+    is_io_bound=False,
+    burst_time=5
+)
 
-# TODO: Create object process
-# my_process = HighLevelProcess()
-# process_manager_fcfs.put_into_queue_ready(my_process)
+process_manager = HighLevelProcessManagerSRTF([my_process_1,my_process_2,my_process_3])
+process_manager.start()
--- processing_simulator/process.py
+++ processing_simulator/process.py
@@ -1,18 +1,15 @@
-from multiprocessing import Process
 import time
 
-class HighLevelProcess(Process):
+class HighLevelProcess():
     def __init__(self,
-                 target,
-                 args,
                  pid: int,
                  name: str,
                  priority: int,
                  is_io_bound: bool,
                  burst_time: int
     ) -> None:
-        super().__init__(target=target, args=args, name=name)
-        self.false_pid = pid
+        self.pid = pid
+        self.name = name
         self.priority = priority
         self.is_io_bound = is_io_bound
         self.burst_time = burst_time
@@ -23,7 +20,7 @@ class HighLevelProcess(Process):
         """Set arrival time as current time
         """
         self.arrival_time = time.time()
-        
+
     def get_execution_time(self) -> float:
         """Get execution time based on current time subtracted to arrival_time
         (start_time)
