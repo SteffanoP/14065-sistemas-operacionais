69b590b10b43cd85b91ef4fbc4f082f4b7fb3ad6
Julyanne Correia <julyanne.es@gmail.com>
2023-07-22 17:49:25 -0300

    fix: arrival_time

--- memory_allocation/main.py
+++ memory_allocation/main.py
@@ -1,10 +1,11 @@
 from process import Process
-from process_manager import ProcessManager
+from process_manager import ProcessFirstFitAlgorithm
 
 process_1 = Process(
-    pid=1,
+    pid=0,
     name="P1",
     size=3,
+    arrival_time=0,
     burst_time=0
 )
 
@@ -12,22 +13,25 @@ process_2 = Process(
     pid=1,
     name="P2",
     size=4,
+    arrival_time=0,
     burst_time=0
 )
 
 process_3 = Process(
-    pid=1,
+    pid=2,
     name="P3",
     size=2,
+    arrival_time=0,
     burst_time=0
 )
 
 process_4 = Process(
-    pid=1,
+    pid=3,
     name="P4",
     size=1,
+    arrival_time=0,
     burst_time=0
 )
 
-process_manager = ProcessManager([process_1, process_2, process_3, process_4])
+process_manager = ProcessFirstFitAlgorithm([process_1, process_2, process_3, process_4])
 process_manager.start()
