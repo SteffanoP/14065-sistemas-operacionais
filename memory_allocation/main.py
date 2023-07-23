5e7f4d6da8576787649888f7412e64d45764122b
SteffanoP <steffanoxpereira@gmail.com>
2023-07-23 19:39:45 -0300

    refactor: remove unused attributes from Process

--- memory_allocation/main.py
+++ memory_allocation/main.py
@@ -8,33 +8,31 @@ processes_created = []
 process_1 = Process(
     pid=0,
     name="P1",
-    size=212,
-    arrival_time=0,
-    burst_time=0
+    size=212
 )
 
 process_2 = Process(
     pid=1,
     name="P2",
-    size=417,
-    arrival_time=0,
-    burst_time=0
+    size=417
 )
 
 process_3 = Process(
     pid=2,
     name="P3",
-    size=112,
-    arrival_time=0,
-    burst_time=0
+    size=112
 )
 
 process_4 = Process(
     pid=3,
     name="P4",
-    size=426,
-    arrival_time=0,
-    burst_time=0
+    size=426
+)
+
+process_5 = Process(
+    pid=4,
+    name="P5",
+    size=599
 )
 
 memory_manager_first_fit = MemoryFirstFitAlgorithm([process_1, process_2, process_3, process_4])
--- memory_allocation/process.py
+++ memory_allocation/process.py
@@ -2,12 +2,8 @@ class Process():
     def __init__(self,
                  pid: int,
                  name: str,
-                 size: int, ## MB
-                 arrival_time: int,
-                 burst_time: int,
+                 size: int ## MB
     ) -> None:
         self.pid = pid
         self.name = name
         self.size = size
-        self.arrival_time = arrival_time
-        self.burst_time = burst_time
