bfb26f9009638531c3aef33ecc63e6b9758ad5d7
SteffanoP <steffanoxpereira@gmail.com>
2023-09-10 09:15:40 -0300

    Atividade 10 - Brute forcing passwords

---
+++ seguranca_crypto/brute_forcing.py
@@ -0,0 +1,14 @@
+your_list = 'tes'
+complete_list = []
+for current in range(5):
+    a = list(your_list)
+    for y in range(current):
+        a = [x+i for i in your_list for x in a]
+
+    complete_list = complete_list+a
+
+for word, pos in zip(complete_list, range(100000)):
+    if word == 'test':
+        print('A senha é: '+word)
+        print('Posição: '+str(pos))
+        break
